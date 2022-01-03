22cf8dc4c652f48db7e1e2baaf3e0f28c680f93d
Dennis Mutuma <dennocapi@gmail.com>
2022-01-03 13:40:54 +0300

    port

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/meterReadings', meterReadingRoute)
 mongoose.connect(process.env.DB_CONNECTION, (err) => {
     console.log(err)
 })
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 });
