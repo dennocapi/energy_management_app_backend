64cf23a222fcae57dd02af7efae8694a837cb24f
Dennis Mutuma <dennocapi@gmail.com>
2022-03-26 11:05:35 +0300

    Cors links

--- app.js
+++ app.js
@@ -12,13 +12,9 @@ const meterReadingRoute = require('./routes/meterReadingRoute')
 const { requireUser } = require("./middleware/requireUser")
 
 const app = express()
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin",  ["https://energy-management-app-backend.herokuapp.com", "https://energy-management-software.herokuapp.com", "http://localhost:3000", "http://localhost:5000","https://nervous-goldwasser-ea1a45.netlify.app", "https://goofy-jang-79f2b5.netlify.app"])
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//     next()
-//   })
+
 app.use(cors({
-    origin:  ["https://energy-management-app-backend.herokuapp.com", "https://energy-management-software.herokuapp.com", "http://localhost:3000", "http://localhost:5000","https://nervous-goldwasser-ea1a45.netlify.app", "https://goofy-jang-79f2b5.netlify.app"],
+    origin: ["https://energy-management-app-backend.herokuapp.com", "https://energy-management-software.herokuapp.com", "http://localhost:3000", "http://localhost:5000","https://nervous-goldwasser-ea1a45.netlify.app", "https://goofy-jang-79f2b5.netlify.app"],
     credentials: true
 }))
 app.use(express.json())
